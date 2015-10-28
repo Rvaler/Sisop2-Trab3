78872174a5092c57e6d819b77a0a72bb3964a8c4
Rafael Valer <rvaler93@gmail.com>
2015-10-28 00:12:57 -0200

    receiving messages

--- client.c
+++ client.c
@@ -9,13 +9,27 @@
 #include <netdb.h>
 #include <pthread.h>
 
-#define MESSAGE_SIZE 256
+#include <arpa/inet.h>
 
+#define NICKLENGHT 32
+#define MESSAGE_SIZE 256
+#define ERROR -1
+
+int isConnected = 0;
+
+char option[256];
 int socket_desc;
 int port;
 struct hostent *server;
 pthread_t receiverThread, senderThread;
 //char *inAddress = "201.21.215.114";
+
+// put this in .h later
+struct PACKET {
+	int roomID;
+	char nickname[NICKLENGHT];
+	char buffer[MESSAGE_SIZE];
+};
 
 
 int connectToServer(int argc, char *argv[]);
@@ -24,14 +38,24 @@ void *messageSender(void *arg);
 
 int main(int argc , char *argv[])
 {
+	
 	if(verifyInput(argc, argv) < 0)
-		return -1;
+		return ERROR;
 
 	if(connectToServer(argc, argv) == -1){
 		puts("Error in connection");
-		return -1;
+		return ERROR;
 	}
 
+
+	
+/*
+	while(gets(option)){ // TODO: implementar o menu aqui
+		if(!strncmp(option, "exit", 4)) {
+			puts("entrou");
+		}
+	}
+	*/
 
 	pthread_create(&senderThread, NULL, messageSender, NULL);
 	pthread_create(&receiverThread, NULL, messageReceiver, NULL);
@@ -40,99 +64,69 @@ int main(int argc , char *argv[])
 	pthread_join(receiverThread, NULL);
 
 	close(socket_desc);
-	/*
-	int socket_desc;
-	struct sockaddr_in server;
-	char *message, server_reply[2000];
-	
-	//Create socket
-	socket_desc = socket(AF_INET , SOCK_STREAM , 0);
-	if (socket_desc < 0)
-	{
-		printf("Could not create socket");
-	}
-		
-	server.sin_addr.s_addr = inet_addr(google);
-	server.sin_family = AF_INET;
-	server.sin_port = htons( 80 );
 
-	//Connect to remote server
-	if (connect(socket_desc , (struct sockaddr *)&server , sizeof(server)) < 0)
-	{
-		puts("connect error");
-		return 1;
-	}
-	puts("Connected");
-
-
-	//Send some data
-	message = "GET / HTTP/1.1\r\n\r\n";
-	if( send(socket_desc , message , strlen(message) , 0) < 0)
-	{
-		puts("Send failed");
-		return 1;
-	}
-	puts("Data Send\n");
-	
-	//Receive a reply from the server
-	if( recv(socket_desc, server_reply , 2000 , 0) < 0)
-	{
-		puts("recv failed");
-	}
-	puts("Reply received\n");
-	puts(server_reply);
-
-	close(socket_desc);
-	puts("Closed socket");
-	*/
 	return 0;
 }
 
 int verifyInput(int argc, char *argv[]) {
 	if(argc < 3) {
 		printf("Error - Missing arguments!\nUse:./client hostname portNumber\n");
-		return -1;
+		return ERROR;
 	}
 }
 
 int connectToServer(int argc, char *argv[]) {
 
-	struct sockaddr_in server_addr;
+	struct sockaddr_in serv_addr;
 	port = atoi(argv[2]);
-	server = gethostbyname(argv[1]);
 
-	socket_desc = socket(AF_INET , SOCK_STREAM , 0);
-	if (socket_desc < 0)
-	{
-		printf("Could not create socket");
-		return -1;
+	// gerando endereços de acordo com a entrada do host indicado pelo usuario
+	if((server = gethostbyname(argv[1])) == NULL) {
+		puts("Error - gethostbyname()");
+		return ERROR;	
 	}
 
-	server_addr.sin_addr = *((struct in_addr *)server->h_addr);
-	server_addr.sin_family = AF_INET;
-	server_addr.sin_port = htons( port );
+	// criação do socket 
+	if((socket_desc = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+		printf("Could not create socket");
+		return ERROR;	
+	}
+
+	// configuração inicial do descritor
+	serv_addr.sin_addr = *((struct in_addr *)server->h_addr);
+	serv_addr.sin_family = AF_INET;
+	serv_addr.sin_port = htons( port );
+	memset(&(serv_addr.sin_zero), 0, 8);
 
 	//Connect to remote server
-	if (connect(socket_desc , (struct sockaddr *)&server_addr , sizeof(server_addr)) < 0)
-	{
+	if (connect(socket_desc , (struct sockaddr *)&serv_addr , sizeof(serv_addr)) < 0) {
 		puts("connect error");
-		return -1;
+		return ERROR;
+	} else {
+		puts("Connected");
+		isConnected = 1;
+		return socket_desc;	
 	}
-	puts("Connected");
-	return 0;
 }
 
 void *messageSender(void *arg){
 }
 
 void *messageReceiver(void *arg){
-	char buffer[MESSAGE_SIZE];
-	//Receive a reply from the server
-	if( recv(socket_desc, buffer , MESSAGE_SIZE , 0) < 0)
-	{
-		puts("recv failed");
-	}
-	puts(buffer);
+	int receivedMessage;
+	struct PACKET receivedPacket;
 
-    pthread_exit(0);
+	while(isConnected) {
+		receivedMessage = recv(socket_desc, (void *)&receivedPacket, sizeof(struct PACKET) , 0);
+		if(!receivedMessage) {
+			puts("Connection with server terminated");
+			isConnected = 0;
+			pthread_exit(0);
+		}	
+		if(receivedMessage > 0){
+			printf("%s: %s \n", receivedPacket.nickname, receivedPacket.buffer);
+		}
+		memset(&receivedPacket, 0, sizeof(struct PACKET));
+	}
+	pthread_exit(0);
 }
